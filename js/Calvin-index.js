5b3417a0ead210a96cbdf94b0bd94713562efe27
= <=>
2016-06-05 18:42:58 +0800

    Signed-off-by: = <=>最终合成

--- js/Calvin-index.js
+++ js/Calvin-index.js
@@ -1,20 +1,33 @@
-var detail_inf = ["【3D爆改】Dubstepast", "IVHQ国际乱七八糟组织",
-                     "歌词不忍直视 R&B界的奇葩", "让你摇晃摇晃",
+var detail_inf = [
+                  "【3D爆改】Dubstepast", "IVHQ国际乱七八糟组织",
+                  "歌词不忍直视 R&B界的奇葩", "让你摇晃摇晃",
                   "开口酥女声搭配日本什么乱七八糟的歌", "霓虹流行乐",
                   "Maroon 5 - Moves Like Jagger", "我的男神",
                   "开口脆系列", "那些碉堡的神曲",
                   "前奏逼死强迫症 Sky Says Something", "逼死强迫症系列",
                   "别人家的粉丝自制天籁神曲", "我是粉粉粉粉粉",
-                  "别戳进来了，这个是作数的", "你猜猜猜"
+                  "别戳进来了,这个是作数的", "你猜猜猜"
                    ],
-    img_src = ["img/1.jpg", "img/2.jpg", "img/3.jpg",
+    img_src = [
+                "img/1.jpg", "img/2.jpg", "img/3.jpg",
                 "img/4.jpg", "img/5.jpg", "img/6.jpg",
                 "img/7.jpg", "img/8.jpg", "img/9.jpg",
                 "img/10.jpg", "img/11.jpg", "img/12.jpg",
                 "img/13.jpg", "img/14.jpg", "img/15.jpg"
-              ];
+              ],
+    music_src = [
+                 "music/1.mp3",
+                 "music/2.mp3",
+                 "music/3.mp3",
+                 "music/4.mp3",
+                 "music/5.mp3",
+                 "music/6.mp3",
+                 "music/7.mp3",
+                 "music/8.mp3"
+                ];
 
 $(document).ready(function(){
+    var audio = document.createElement("audio");
     var $advice_btn = $(".calvin .music-advice .advice-btn"),
         $img_holders = $(".calvin .music-advice .img-holders"),
         $img_holder = $(".calvin .img-holders .img-holder");
@@ -23,10 +36,9 @@ $(document).ready(function(){
         $music_stage = $(".miniplayer span.stage"),
         music_name = "无正在播放";
     $music_name.text(music_name);
-    /*点击图片播放音乐事件*/
+    
     var $music_img = $img_holder.children()//.row
                                      .children("div.img").children(); //.img
-    
     /*miniplayer的出现隐藏*/
     var $miniplayer = $(".head .miniplayer"),
         $icon_player = $("img.icon-player");
@@ -40,18 +52,26 @@ $(document).ready(function(){
     $miniplayer.children().eq(1).click(function() {
         var icon_src = ["img/stop.png", "img/play.png"],
             $icon_src = $miniplayer.children().eq(1).children().attr("src");
-        if($icon_src === icon_src[0])
+        
+        if($icon_src === icon_src[0]) {
             $miniplayer.children().eq(1).children().attr("src", icon_src[1]);
+            
+            audio.pause();
+        }
         else {
             $miniplayer.children().eq(1).children().attr("src", icon_src[0]);
             
             /**********************页面切换**********************/
-            //window.location.href="Jamie-player.html";
+
             $(".pt-page-moveToRight").removeClass("pt-page-moveToRight");
             $(".invisible").addClass("pt-page-moveToLeft");
             //$(".index").addClass("invisible");
             $(".index").fadeOut(500);
             $(".Jplayer").removeClass("invisible");
+            $("#player-musictitle").text(music_name);
+            
+            /*音乐播放*/
+            audio.play();
         }
     });
     
@@ -62,7 +82,7 @@ $(document).ready(function(){
             offset = 1;
         /*改变给张图片的src位置*/
         for(var i=0; i<8; i++) {
-            /*获取img对象， 计算出随机的src*/
+            /*获取img对象, 计算出随机的src*/
             var $img = $img_holder.children().eq(i)        //.row
                                      .children("div.img").children(); //.img
             /*获取detail对象*/
@@ -84,14 +104,21 @@ $(document).ready(function(){
         $img_holders.fadeIn("fast");
     });
     
-    /*更改播放曲目名称*/
+    /*点击图片更改播放曲目名称*/
     $music_img.click(function() {
         $miniplayer.slideDown();
         //alert($(this).parent().parent().parent().index());
         music_name = $(this).parent().next().children().first().text();
         //alert(music_name);
         $music_name.text(music_name);
+        $miniplayer.children().eq(1).children().attr("src", "img/play.png");
         //window.location.href="Jamie-player.html";
+        var $img_src = $(this).attr("src"),
+            $dm_bgimg = $(".player-middle div img");
+        $dm_bgimg.attr("src", $img_src);
+        
+        audio.pause();
+        audio.src = music_src[$(this).parent().parent().parent().index()];
     });
     
     /*返回index*/
@@ -103,4 +130,19 @@ $(document).ready(function(){
         //$(".index").removeClass("invisible");
         $(".index").fadeIn();
     });
+    
+    /*关注按钮*/
+    var $guanzhu_btn = $("button.guanzhu");
+    $("button.guanzhu").click(function() {
+        if($guanzhu_btn.text() === "+关注") {
+            $guanzhu_btn.text("已关注");
+            $("span.player-attention-green").text("22223");
+        }
+        else {
+            $guanzhu_btn.text("+关注");
+            $("span.player-attention-green").text("22222");
+        }
+    });
+    
+    
 });
